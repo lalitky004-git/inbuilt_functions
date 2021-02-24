e55368666fbbac0f2c7384b88984ffc480fe2a6a
lalitky004-git <78463411+lalitky004-git@users.noreply.github.com>
2021-02-24 23:07:53 +0530

    Update 19-2_cookie.php

--- inbuilt/19-2_cookie.php
+++ inbuilt/19-2_cookie.php
@@ -6,12 +6,12 @@ setcookie($cookie_name, $cookie_value, time() + (86400 *30), "/");
 <html>
 <body>
 <?php
-if(!isset($COOKIE1[$cookie_name])){
+if(!isset($_COOKIE[$cookie_name])){
     echo "Cookie is named'" . $cookie_name . "' is not set!";
 }
 else{
     echo "Cookie '" . $cookie_name . "' is set!<br>";
-    echo "value is:"  . $COOKIE1($cookie_name);
+    echo "value is:"  . $_COOKIE($cookie_name);
 }
 ?>
 </body>
